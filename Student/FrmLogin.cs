5aab4fb5ea9e7568eff4213850629fb3a59bb702
chenxiu <Administrator@DEEP-20161031MN>
2017-04-11 15:37:32 +0800

    完成登录界面

--- Student/FrmLogin.cs
+++ Student/FrmLogin.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Student
 {
@@ -19,11 +20,34 @@ namespace Student
         private DBHelper helper = new DBHelper();
         private void btnEnter_Click(object sender, EventArgs e)
         {
-            string userName = this.txtUserName.Text.Trim ();
+            string userName = this.txtUserName.Text.Trim();
             string key = this.txtKey.Text.Trim();
-            string strSQL = @"select log";
-            int row = Convert.ToInt32(helper.ExecuteNonQuery(strSQL, CommandType.Text, new SqlParameter("@StudentNo", StuNo),
-         
+            int i = 0;
+            string password = "";
+            string strSQL = @"select LoginPwd from student where LoginId=@LoginId";
+            using (IDataReader reader = helper.ExecuteReader(strSQL, CommandType.StoredProcedure, new SqlParameter("@LoginId", userName)))
+            {
+                if(reader.Read())
+                {
+                    password = reader.GetString(reader.GetOrdinal("LoginPwd"));
+                    i++;
+                }
+            }
+            if (i==0)
+            {
+                MessageBox.Show("用户名不存在！");
+            }
+            else
+            {
+                if (key== password)
+                {
+                    MessageBox.Show("登录成功");
+                }
+                else
+                {
+                    MessageBox.Show("您的密码不正确！");
+                }
+            }
         }
     }
 }
