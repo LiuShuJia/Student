ccc63b4a0d667b5754b50375b42cebf702ec7428
Ejfiou <1369540047@qq.com>
2017-04-11 15:01:10 +0800

    创建了DBHelper类

---
+++ Student/DBHelper.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Student
+{
+    class DBHelper
+    {
+        public DBHelper()
+        {
+            con = new SqlConnection(strCon);
+            cmd = new SqlCommand();
+        }
+
+        private static string strCon = @"server=.\sql2014;database=MySchool;uid=sa;password=123";
+        private IDbConnection con = null;
+        private IDbCommand cmd = null;
+
+        public object ExecuteScalar(string strSQL, CommandType commandType = CommandType.Text, params IDataParameter[] parameters)
+        {
+            object obj = null;
+
+            try
+            {
+                this.cmd.CommandText = strSQL;
+                this.cmd.CommandType = commandType;
+                this.cmd.Parameters.Clear();
+                foreach (var item in parameters)
+                {
+                    this.cmd.Parameters.Add(item);
+                }
+
+                this.con.Open();
+
+                obj = this.cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                this.con.Close();
+
+            }
+
+            return obj;
+
+        }
+
+
+        public int ExecuteNonQuery(string strSQL, CommandType commandType = CommandType.Text, params IDataParameter[] parameters)
+        {
+            int rows = 0;
+
+            try
+            {
+                this.cmd.CommandText = strSQL;
+                this.cmd.CommandType = commandType;
+                this.cmd.Parameters.Clear();
+                foreach (var item in parameters)
+                {
+                    this.cmd.Parameters.Add(item);
+                }
+
+                this.con.Open();
+
+                rows = this.cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                this.con.Close();
+
+            }
+
+            return rows;
+
+        }
+
+        public IDataReader ExecuteReader(string strSQL, CommandType commandType = CommandType.Text, params IDataParameter[] parameters)
+        {
+            IDataReader reader = null;
+            try
+            {
+                this.cmd.CommandText = strSQL;
+                this.cmd.CommandType = commandType;
+                this.cmd.Parameters.Clear();
+                foreach (var item in parameters)
+                {
+                    this.cmd.Parameters.Add(item);
+                }
+
+                this.con.Open();
+
+                reader = this.cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            return reader;
+
+        }
+
+    }
+}
--- Student/Form1.Designer.cs
+++ Student/Form1.Designer.cs
@@ -28,20 +28,224 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.lvwShow = new System.Windows.Forms.ListView();
+            this.LoginId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.UserStates = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ClassName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.StudentNo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.StudentName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Sex = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Address = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.cboClass = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboSex = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tmsMain = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmiModify = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiDel = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiModifyPwd = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmsMain.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // lvwShow
+            // 
+            this.lvwShow.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.LoginId,
+            this.UserStates,
+            this.ClassName,
+            this.StudentNo,
+            this.StudentName,
+            this.Sex,
+            this.Address});
+            this.lvwShow.FullRowSelect = true;
+            this.lvwShow.GridLines = true;
+            this.lvwShow.Location = new System.Drawing.Point(12, 143);
+            this.lvwShow.Name = "lvwShow";
+            this.lvwShow.Size = new System.Drawing.Size(425, 269);
+            this.lvwShow.TabIndex = 17;
+            this.lvwShow.UseCompatibleStateImageBehavior = false;
+            this.lvwShow.View = System.Windows.Forms.View.Details;
+            // 
+            // LoginId
+            // 
+            this.LoginId.Text = "登录账号";
+            // 
+            // UserStates
+            // 
+            this.UserStates.Text = "用户状态";
+            // 
+            // ClassName
+            // 
+            this.ClassName.Text = "班级名";
+            // 
+            // StudentNo
+            // 
+            this.StudentNo.Text = "学号";
+            // 
+            // StudentName
+            // 
+            this.StudentName.Text = "姓名";
+            // 
+            // Sex
+            // 
+            this.Sex.Text = "性别";
+            // 
+            // Address
+            // 
+            this.Address.Text = "地址";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(299, 70);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 16;
+            this.btnAdd.Text = "增加学生";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(299, 12);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 15;
+            this.btnSelect.Text = "查询";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            // 
+            // cboClass
+            // 
+            this.cboClass.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboClass.FormattingEnabled = true;
+            this.cboClass.Items.AddRange(new object[] {
+            "查询所有"});
+            this.cboClass.Location = new System.Drawing.Point(105, 101);
+            this.cboClass.Name = "cboClass";
+            this.cboClass.Size = new System.Drawing.Size(121, 20);
+            this.cboClass.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "通过课程查询：";
+            // 
+            // cboSex
+            // 
+            this.cboSex.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboSex.FormattingEnabled = true;
+            this.cboSex.Items.AddRange(new object[] {
+            "男",
+            "女",
+            "查询所有"});
+            this.cboSex.Location = new System.Drawing.Point(105, 56);
+            this.cboSex.Name = "cboSex";
+            this.cboSex.Size = new System.Drawing.Size(121, 20);
+            this.cboSex.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "通过性别查询：";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(105, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(140, 21);
+            this.txtName.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "通过姓名查询：";
+            // 
+            // tmsMain
+            // 
+            this.tmsMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiModify,
+            this.tsmiDel,
+            this.tsmiModifyPwd});
+            this.tmsMain.Name = "contextMenuStrip1";
+            this.tmsMain.Size = new System.Drawing.Size(125, 70);
+            // 
+            // tsmiModify
+            // 
+            this.tsmiModify.Name = "tsmiModify";
+            this.tsmiModify.Size = new System.Drawing.Size(124, 22);
+            this.tsmiModify.Text = "修改";
+            // 
+            // tsmiDel
+            // 
+            this.tsmiDel.Name = "tsmiDel";
+            this.tsmiDel.Size = new System.Drawing.Size(124, 22);
+            this.tsmiDel.Text = "删除";
+            // 
+            // tsmiModifyPwd
+            // 
+            this.tsmiModifyPwd.Name = "tsmiModifyPwd";
+            this.tsmiModifyPwd.Size = new System.Drawing.Size(124, 22);
+            this.tsmiModifyPwd.Text = "修改密码";
             // 
             // FrmSelectStudent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(444, 418);
+            this.Controls.Add(this.lvwShow);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.cboClass);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboSex);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
             this.Name = "FrmSelectStudent";
             this.Text = "学生信息管理界面";
+            this.tmsMain.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView lvwShow;
+        private System.Windows.Forms.ColumnHeader LoginId;
+        private System.Windows.Forms.ColumnHeader UserStates;
+        private System.Windows.Forms.ColumnHeader ClassName;
+        private System.Windows.Forms.ColumnHeader StudentNo;
+        private System.Windows.Forms.ColumnHeader StudentName;
+        private System.Windows.Forms.ColumnHeader Sex;
+        private System.Windows.Forms.ColumnHeader Address;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.ComboBox cboClass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboSex;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip tmsMain;
+        private System.Windows.Forms.ToolStripMenuItem tsmiModify;
+        private System.Windows.Forms.ToolStripMenuItem tsmiDel;
+        private System.Windows.Forms.ToolStripMenuItem tsmiModifyPwd;
     }
 }
 
