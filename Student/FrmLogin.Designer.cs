df411a44df87449399061bfd04154a721e8e55f6
chenxiu <Administrator@DEEP-20161031MN>
2017-04-11 15:11:17 +0800

    完成登录界面

--- Student/FrmLogin.Designer.cs
+++ Student/FrmLogin.Designer.cs
@@ -28,19 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请输入用户名：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "请输入密码：";
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(97, 198);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "确定";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(122, 83);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(100, 21);
+            this.txtUserName.TabIndex = 3;
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(122, 134);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(100, 21);
+            this.txtKey.TabIndex = 4;
             // 
             // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "FrmLogin";
             this.Text = "登录界面";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtKey;
     }
 }
--- Student/FrmLogin.cs
+++ Student/FrmLogin.cs
@@ -16,5 +16,14 @@ namespace Student
         {
             InitializeComponent();
         }
+        private DBHelper helper = new DBHelper();
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            string userName = this.txtUserName.Text.Trim ();
+            string key = this.txtKey.Text.Trim();
+            string strSQL = @"select log";
+            int row = Convert.ToInt32(helper.ExecuteNonQuery(strSQL, CommandType.Text, new SqlParameter("@StudentNo", StuNo),
+         
+        }
     }
 }
