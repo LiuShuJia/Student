593cda016140f3ca88911bdf4a555fe0956a2bee
Administrator <Administrator@DEEP-20161031WY>
2017-04-11 15:11:14 +0800

    设计FrmModifStudent界面

--- Student/FrmModifyStudent.Designer.cs
+++ Student/FrmModifyStudent.Designer.cs
@@ -28,19 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtStudentNo = new System.Windows.Forms.TextBox();
+            this.cboUserState = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cboClassName = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // txtStudentNo
+            // 
+            this.txtStudentNo.Location = new System.Drawing.Point(233, 46);
+            this.txtStudentNo.Name = "txtStudentNo";
+            this.txtStudentNo.Size = new System.Drawing.Size(148, 21);
+            this.txtStudentNo.TabIndex = 67;
+            // 
+            // cboUserState
+            // 
+            this.cboUserState.FormattingEnabled = true;
+            this.cboUserState.Items.AddRange(new object[] {
+            "在线",
+            "离线"});
+            this.cboUserState.Location = new System.Drawing.Point(233, 153);
+            this.cboUserState.Name = "cboUserState";
+            this.cboUserState.Size = new System.Drawing.Size(148, 20);
+            this.cboUserState.TabIndex = 66;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(162, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 65;
+            this.label1.Text = "学生状态：";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(133, 330);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 64;
+            this.btnUpdate.Text = "修改";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(233, 272);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(148, 21);
+            this.txtAddress.TabIndex = 63;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(78, 275);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(149, 12);
+            this.label8.TabIndex = 62;
+            this.label8.Text = "你要修改学生的家庭地址：";
+            // 
+            // cboClassName
+            // 
+            this.cboClassName.FormattingEnabled = true;
+            this.cboClassName.Location = new System.Drawing.Point(233, 217);
+            this.cboClassName.Name = "cboClassName";
+            this.cboClassName.Size = new System.Drawing.Size(148, 20);
+            this.cboClassName.TabIndex = 61;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(102, 220);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(125, 12);
+            this.label6.TabIndex = 60;
+            this.label6.Text = "你要修改学生的班级：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(102, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 12);
+            this.label5.TabIndex = 59;
+            this.label5.Text = "你要修改学生的学号：";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(346, 188);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(35, 16);
+            this.radioButton2.TabIndex = 58;
+            this.radioButton2.Text = "女";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(233, 188);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(35, 16);
+            this.radioButton1.TabIndex = 57;
+            this.radioButton1.Text = "男";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(102, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 12);
+            this.label4.TabIndex = 56;
+            this.label4.Text = "你要修改学生的性别：";
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Location = new System.Drawing.Point(233, 109);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(148, 21);
+            this.txtStudentName.TabIndex = 55;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 12);
+            this.label3.TabIndex = 54;
+            this.label3.Text = "你要修改学生的姓名：";
             // 
             // FrmModifyStudent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(537, 459);
+            this.Controls.Add(this.txtStudentNo);
+            this.Controls.Add(this.cboUserState);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cboClassName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtStudentName);
+            this.Controls.Add(this.label3);
             this.Name = "FrmModifyStudent";
             this.Text = "修改学生信息";
+            this.Load += new System.EventHandler(this.FrmModifyStudent_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtStudentNo;
+        private System.Windows.Forms.ComboBox cboUserState;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cboClassName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Student/FrmModifyStudent.cs
+++ Student/FrmModifyStudent.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace Student
 {
@@ -16,5 +18,51 @@ namespace Student
         {
             InitializeComponent();
         }
-    }
+        public FrmModifyStudent(Form parentForm)
+        {
+            InitializeComponent();
+            this.parentForm = parentForm;
+        }
+        public string guid = null;
+        private Form parentForm;
+        private DBHelper helper = new DBHelper();
+        private string strCon = @"server=DEEP-20161031WY;database=MySchool;uid=sa;password=123;";
+        private void FrmModifyStudent_Load(object sender, EventArgs e)
+        {
+            string strSQL = "sp_classQuery  ";
+            using (IDataReader reader = helper.ExecuteReader(strSQL, CommandType.StoredProcedure))
+            {
+                while (reader.Read())
+                {
+                    string gradename = reader.GetString(reader.GetOrdinal("ClassName"));
+                    this.cboClassName.Items.Add(gradename);
+                }
+                reader.Close();
+            }
+            if (guid != null)
+            {
+                strSQL = "sp_studentQuery ";
+            }
+            using (IDataReader reader = helper.ExecuteReader(strSQL, CommandType.StoredProcedure, new SqlParameter("@StudentGuid", guid)))
+            {
+                if (reader.Read())
+                {
+                    this.txtStudentNo.Text = reader.GetString(reader.GetOrdinal("StudentNO"));
+                    this.txtStudentName.Text = reader.GetString(reader.GetOrdinal("StudentName"));
+                    if (reader.GetString(reader.GetOrdinal("Sex")) == "女")
+                    {
+                        this.radioButton2.Checked = true;
+                    }
+                    else
+                    {
+                        this.radioButton1.Checked = true;
+                    }
+                    this.cboUserState.Text = reader.GetString(reader.GetOrdinal("UserStateId")) == "1" ? "在线" : "离线";
+                    this.txtAddress.Text = reader.GetString(reader.GetOrdinal("Address"));
+                    this.cboClassName.Text = reader.GetString(reader.GetOrdinal("ClassName")); ;
+                }
+                reader.Close();
+            }
+        }
+        }
 }
