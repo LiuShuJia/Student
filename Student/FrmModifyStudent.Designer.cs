9fe21a896372aac67c6cadffe7490f8c5bce5331
Administrator <Administrator@DEEP-20161101LR>
2017-04-11 14:26:24 +0800

    添加项目文件。

---
+++ Student/FrmModifyStudent.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Student
+{
+    partial class FrmModifyStudent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmModifyStudent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "FrmModifyStudent";
+            this.Text = "修改学生信息";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
