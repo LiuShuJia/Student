2c540fb8d3013bfb09f41cfa7b8c52e96ed3fb10
叶贻峰 <Administrator@DEEP-20161031HU>
2017-04-11 15:28:53 +0800

    完成学生添加页面

--- Student/FrmAddStudnet.Designer.cs
+++ Student/FrmAddStudnet.Designer.cs
@@ -28,19 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnYes = new System.Windows.Forms.Button();
+            this.rbtnWoman = new System.Windows.Forms.RadioButton();
+            this.rbtnMan = new System.Windows.Forms.RadioButton();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtStuNo = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboClassGUID = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLoginId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnYes
+            // 
+            this.btnYes.Location = new System.Drawing.Point(89, 268);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(75, 23);
+            this.btnYes.TabIndex = 27;
+            this.btnYes.Text = "确定";
+            this.btnYes.UseVisualStyleBackColor = true;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // rbtnWoman
+            // 
+            this.rbtnWoman.AutoSize = true;
+            this.rbtnWoman.Location = new System.Drawing.Point(151, 191);
+            this.rbtnWoman.Name = "rbtnWoman";
+            this.rbtnWoman.Size = new System.Drawing.Size(35, 16);
+            this.rbtnWoman.TabIndex = 26;
+            this.rbtnWoman.TabStop = true;
+            this.rbtnWoman.Text = "女";
+            this.rbtnWoman.UseVisualStyleBackColor = true;
+            // 
+            // rbtnMan
+            // 
+            this.rbtnMan.AutoSize = true;
+            this.rbtnMan.Checked = true;
+            this.rbtnMan.Location = new System.Drawing.Point(101, 191);
+            this.rbtnMan.Name = "rbtnMan";
+            this.rbtnMan.Size = new System.Drawing.Size(35, 16);
+            this.rbtnMan.TabIndex = 25;
+            this.rbtnMan.TabStop = true;
+            this.rbtnMan.Text = "男";
+            this.rbtnMan.UseVisualStyleBackColor = true;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(101, 222);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(121, 21);
+            this.txtAddress.TabIndex = 24;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(101, 149);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(121, 21);
+            this.txtName.TabIndex = 23;
+            // 
+            // txtStuNo
+            // 
+            this.txtStuNo.Location = new System.Drawing.Point(101, 101);
+            this.txtStuNo.Name = "txtStuNo";
+            this.txtStuNo.Size = new System.Drawing.Size(121, 21);
+            this.txtStuNo.TabIndex = 22;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "住址：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "性别：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "姓名：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "学号：";
+            // 
+            // cboClassGUID
+            // 
+            this.cboClassGUID.FormattingEnabled = true;
+            this.cboClassGUID.Location = new System.Drawing.Point(101, 62);
+            this.cboClassGUID.Name = "cboClassGUID";
+            this.cboClassGUID.Size = new System.Drawing.Size(121, 20);
+            this.cboClassGUID.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "班级：";
+            // 
+            // txtLoginId
+            // 
+            this.txtLoginId.Location = new System.Drawing.Point(101, 14);
+            this.txtLoginId.Name = "txtLoginId";
+            this.txtLoginId.Size = new System.Drawing.Size(121, 21);
+            this.txtLoginId.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "用户名：";
             // 
             // FrmAddStudnet
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(296, 337);
+            this.Controls.Add(this.btnYes);
+            this.Controls.Add(this.rbtnWoman);
+            this.Controls.Add(this.rbtnMan);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtStuNo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboClassGUID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLoginId);
+            this.Controls.Add(this.label1);
             this.Name = "FrmAddStudnet";
             this.Text = "添加学生信息";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.RadioButton rbtnWoman;
+        private System.Windows.Forms.RadioButton rbtnMan;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtStuNo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboClassGUID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLoginId;
+        private System.Windows.Forms.Label label1;
     }
 }
